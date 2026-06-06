26931a04189deb07dc9ee09066baa51d99230aa4
Gugens <nero.n262013@gmail.com>
2026-06-06 21:38:53 +0300

    model Post create

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(
+        max_length=50,
+        verbose_name='Заголовок',
+        help_text='Введите заголовок поста'
+    )
+    content = models.TextField(
+        verbose_name='Контент',
+        help_text='Напишите ваш пост'
+    )
+    preview = models.ImageField(
+        upload_to='blog/images/',
+        blank=True,
+        null=True,
+        verbose_name='Изображение',
+        help_text='Изображение для поста'
+    )
+    post_date = models.DateField(
+        verbose_name='Дата публикации',
+        help_text='Укажите дату публикации',
+        default=date.today,
+    )
+    is_published = models.BooleanField(
+        default=False,
+        verbose_name='Опубликован',
+        help_text='Отметьте, если пост опубликован'
+    )
+    views_counter = models.PositiveIntegerField(
+        verbose_name = "Счетчик просмотров",
+        help_text = "Укажите количество просмотров",
+        default=0,
+    )
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+
+# class PostCreateView(CreateView):
+#     model = Post
+#
+
+# class PostListView(ListView):
+#
+#
+# class PostDetailView(DetailView):
+#
+#
+# class PostUpdateView(UpdateView):
+#
+#
+# class PostDeleteView(DeleteView):
